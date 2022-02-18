bc988b1195626e74dd944d77dd24a0a28a8684cc
TomG1993 <gorton.tom@hotmail.co.uk>
2022-02-18 13:28:04 +0000

    Added new variable

--- RepCalculator/Program.cs
+++ RepCalculator/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var CurrentReps = new List<int>();
+
+CurrentReps.Add(8);
+CurrentReps.Add(8);
+CurrentReps.Add(5);
+
+
+
+Console.WriteLine("Added line");
+
+Console.ReadLine();
